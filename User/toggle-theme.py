848fd63f3324f9a9079f1447302b29888442fef9
Piotr Kazmierczak <me@piotrkazmierczak.com>
2014-06-16 22:36:12 +0200

    switching from themes and color schemes for ST2 now

--- User/toggle-theme.py
+++ User/toggle-theme.py
@@ -1,16 +1,22 @@
 import sublime, sublime_plugin
-
+ 
 class ToggleThemeCommand(sublime_plugin.TextCommand):
     def run(self, edit, **args):
-
-        scheme1 = args["color_scheme_1"]
-        scheme2 = args["color_scheme_2"]
-        theme1  = args["theme_1"]
-        theme2  = args["theme_2"]
-        current_scheme = self.view.settings().get("color_scheme")
-        current_theme  = self.view.settings().get("theme")
-
-        new_scheme = scheme1 if current_scheme == scheme2 else scheme2
-        self.view.settings().set("color_scheme", new_scheme)
-        new_theme  = theme1 if current_theme  == theme2 else theme2
-        self.view.settings().set("theme", new_theme)
+        s = sublime.load_settings("Preferences.sublime-settings")
+        options = args['options']
+        if options[0]:
+            current_scheme = s.get("color_scheme", options[0]['color_scheme'])
+            current_theme  = s.get("theme", options[0]['theme'])
+            current_option = s.get("color_scheme_option", -1)
+ 
+            next_option = current_option + 1
+            if next_option >= len(options):
+                next_option = 0
+ 
+            if options[next_option]['theme']:
+                s.set('theme', options[next_option]['theme'])
+            if options[next_option]['color_scheme']:
+                s.set('color_scheme', options[next_option]['color_scheme'])
+            s.set('color_scheme_option', next_option)
+ 
+            sublime.save_settings("Preferences.sublime-settings")
