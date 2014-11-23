e5622069defb92189da51c1ac1819fd85f6de7c2
pkazmierczak <me@piotrkazmierczak.com>
2014-11-23 14:40:20 +0100

    ST3 configs

--- User/toggle-theme.py
+++ User/toggle-theme.py
@@ -1,5 +1,7 @@
-import sublime, sublime_plugin
- 
+import sublime
+import sublime_plugin
+
+
 class ToggleThemeCommand(sublime_plugin.TextCommand):
     def run(self, edit, **args):
         s = sublime.load_settings("Preferences.sublime-settings")
@@ -8,15 +10,15 @@ class ToggleThemeCommand(sublime_plugin.TextCommand):
             current_scheme = s.get("color_scheme", options[0]['color_scheme'])
             current_theme  = s.get("theme", options[0]['theme'])
             current_option = s.get("color_scheme_option", -1)
- 
+
             next_option = current_option + 1
             if next_option >= len(options):
                 next_option = 0
- 
+
             if options[next_option]['theme']:
                 s.set('theme', options[next_option]['theme'])
             if options[next_option]['color_scheme']:
                 s.set('color_scheme', options[next_option]['color_scheme'])
             s.set('color_scheme_option', next_option)
- 
+
             sublime.save_settings("Preferences.sublime-settings")
