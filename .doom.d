be164383c32b60aaf2d98267c0ca5cf2bb5ff351
Piotr Kazmierczak <piotr@messagebird.com>
2019-07-22 19:17:45 +0200

    dooooooooooooooooooooooom

--- .doom.d
+++
@@ -1,1 +0,0 @@
-/Users/piotr/.doom.d
---
+++ .doom.d/config.el
@@ -0,0 +1,21 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(require 'doom-themes)
+
+;;; Settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t  ; if nil, italics is universally disabled
+
+      ;; doom-one specific settings
+      doom-one-brighter-modeline nil
+      doom-one-brighter-comments nil)
+
+;; Load the theme (doom-one, doom-dark, etc.)
+(load-theme 'doom-solarized-light t)
+
+;; Enable custom neotree theme
+(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+(setq doom-font (font-spec :family "Source Code Pro for Powerline" :size 13))
