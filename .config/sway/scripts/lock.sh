587839442a8a3741bd2d7d278b957909658e3a14
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2026-09-04 20:46:55 +0200

    better lock

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -7,7 +7,7 @@ trap 'rm -rf "$dir"' EXIT
 args=()
 for out in $(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name'); do
   grim -o "$out" "$dir/$out.png"
-  magick "$dir/$out.png" -scale 5% -scale 2000% "$dir/$out.png"
+  magick "$dir/$out.png" -scale 5% -scale 2000% -font 'Source-Code-Pro' -gravity center -pointsize 100 -fill blue -annotate 0 'sod off' "$dir/$out.png"
   args+=(-i "$out:$dir/$out.png")
 done
 
